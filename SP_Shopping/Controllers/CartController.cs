05b8de872e4490124961e02e730ff15c8726d5ba
syildizz <saygin.efe@hotmail.com>
2024-10-03 14:17:51 +0300

    Added create action to cart view to add product to user's cart.

--- SP_Shopping/Controllers/CartController.cs
+++ SP_Shopping/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SP_Shopping.Data;
+using SP_Shopping.Models;
 using System.Security.Claims;
 using System.Text.RegularExpressions;
 
@@ -36,4 +37,26 @@ public class CartController(ApplicationDbContext context) : Controller
         return View(model: id);
     }
 
+    [HttpPost()]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(int productId)
+    {
+        string? userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        bool userExists = _context.Users.Any(u => u.Id == userId);
+        bool productExists = _context.Products.Any(p => p.Id == productId);
+        // Check that the keys are valid.
+        if (!userExists || !productExists)
+        {
+            return NotFound("Product and user specified does not exist..");
+        }
+        CartItem cartItem = new()
+        {
+            UserId = userId!,
+            ProductId = productId
+        };
+        await _context.CartItems.AddAsync(cartItem);
+        int savedNum = await _context.SaveChangesAsync();
+
+        return View(nameof(Index));
+    }
 }
