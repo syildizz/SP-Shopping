93d54916245f7e6d2d3d3c76bd625cfda6e79c2e
syildizz <saygin.efe@hotmail.com>
2024-10-03 11:17:01 +0300

    Added controller and view for Cart.

---
+++ SP_Shopping/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SP_Shopping.Data;
+using System.Security.Claims;
+using System.Text.RegularExpressions;
+
+namespace SP_Shopping.Controllers;
+
+public class CartController(ApplicationDbContext context) : Controller
+{
+
+    private readonly ApplicationDbContext _context = context; 
+
+    public IActionResult Index(string? id)
+    {
+        if (!string.IsNullOrEmpty(id) && Regex.IsMatch(id, @"[sS]elf"))
+        {
+            id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+        return View(model: id);
+    }
+}
