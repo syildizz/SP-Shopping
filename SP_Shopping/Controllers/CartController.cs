69d64a986628e32958b4c2ea54a0f9a57403ea36
syildizz <saygin.efe@hotmail.com>
2024-10-07 17:20:50 +0300

    Add logging to CartController.

--- SP_Shopping/Controllers/CartController.cs
+++ SP_Shopping/Controllers/CartController.cs
@@ -22,6 +22,7 @@ public static class Glob
 
 public class CartController
 (
+    ILogger<CartController> logger,
     IMapper mapper,
     IRepository<CartItem> cartItemRepository,
     IRepository<ApplicationUser> userRepository,
@@ -30,6 +31,7 @@ public class CartController
 ) : Controller
 {
 
+    private readonly ILogger<CartController> _logger = logger;
     private readonly IMapper _mapper = mapper;
     private readonly IMemoryCache _memoryCache = memoryCache;
     private readonly IRepository<CartItem> _cartItemRepository = cartItemRepository;
@@ -39,9 +41,11 @@ public class CartController
     [Authorize]
     public async Task<IActionResult> Index()
     {
+        _logger.LogInformation("GET: Cart/Index");
         string? userName = User.FindFirstValue(ClaimTypes.Name);
         if (userName == null)
         {
+            _logger.LogError("Anonymous user logged in.");
             ViewBag.Message = "You need to log in to see your cart";
         }
         else
@@ -52,10 +56,12 @@ public class CartController
         string? userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
         if (string.IsNullOrWhiteSpace(userId))
         {
+            _logger.LogError("UserId does not exist i.e. no user is logged in.");
             return View("Error");
         }
         string cacheKey = $"CartItemsIndex_{userId}";
 
+        _logger.LogDebug("Generating cancelation token for user with id \"{UserId}\"", userId);
         // Generate Cancellation Token
         if (!Glob.userCancellationTokens.TryGetValue(userId, out CancellationTokenSource? cts))
         {
@@ -63,9 +69,15 @@ public class CartController
             Glob.userCancellationTokens.Add(userId, cts);
         }
 
+        _logger.LogDebug("From cache attempting to get cartitems for user with id \"{UserId}\"", userId);
         IEnumerable<CartItem>? cartItems = await _memoryCache.GetOrCreateAsync(cacheKey, async entry =>
         {
             entry.AddExpirationToken(new CancellationChangeToken(cts.Token));
+            entry.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _logger.LogDebug("CartItems for user \"{UserId}\" has been invalidated due to {Reason} - {Key} : {Value}", userId, reason, key, value);
+            });
+            _logger.LogDebug("Cache miss, from database fetching cartitems for user with id \"{UserId}\"", userId);
             return await _cartItemRepository.GetAllAsync(q => q
                 .Where(c => c.UserId == userId)
                 .Include(c => c.Product)
@@ -88,16 +100,20 @@ public class CartController
     [Route($"/Cart/Index/{{{nameof(id)}}}")]
     public async Task<IActionResult> Index(string? id)
     {
+        _logger.LogInformation("GET: Cart/Index/id (Cart/Index action overloaded).");
         if (!string.IsNullOrEmpty(id) && Regex.IsMatch(id, @"[sS]elf", RegexOptions.Compiled))
         {
+            _logger.LogInformation("Redirecting to main Index since id is {Id}.", id);
             return Redirect($"/Cart/{nameof(Index)}");
         }
         if (string.IsNullOrWhiteSpace(id) || !await _userRepository.ExistsAsync(q => q.Where(u => u.Id == id)))
         {
+            _logger.LogError("UserId does not exist for user with given id of \"{Id}\".", id);
             return NotFound("The user was not found");
         }
 
         // Generate Cancellation Token
+        _logger.LogDebug("Generating cancelation token for user with id \"{UserId}\".", id);
         if (!Glob.userCancellationTokens.TryGetValue(id, out CancellationTokenSource? cts))
         {
             cts = new CancellationTokenSource(new TimeSpan(6,0,0));
@@ -105,9 +121,15 @@ public class CartController
         }
 
         string cacheKey = $"CartItemsIndex_{id}";
+        _logger.LogDebug("From cache attempting to get cartitems for user with id \"{UserId}\".", id);
         IEnumerable<CartItem>? cartItems = await _memoryCache.GetOrCreateAsync($"CartItemsIndex_{id}", async entry =>
         {
             entry.AddExpirationToken(new CancellationChangeToken(cts.Token));
+            entry.RegisterPostEvictionCallback((key, value, reason, state) =>
+            {
+                _logger.LogDebug("CartItems for user \"{UserId}\" has been invalidated due to {Reason} - {Key} : {Value}.", id, reason, key, value);
+            });
+            _logger.LogDebug("Cache miss, from database fetching cartitems for user with id \"{UserId}\".", id);
             return await _cartItemRepository.GetAllAsync(q => q
                 .Where(c => c.UserId == id)
                 .Include(c => c.Product)
@@ -132,18 +154,19 @@ public class CartController
 
     public async Task<IActionResult> Details()
     {
+        _logger.LogInformation("GET: Cart/Details.");
 
-        if (Glob.detailsCancellationToken == null)
-        {
-            Glob.detailsCancellationToken = new CancellationTokenSource(new TimeSpan(6,0,0));
-        }
+        _logger.LogDebug("Generating cancelation token for All users.");
+        Glob.detailsCancellationToken ??= new CancellationTokenSource(new TimeSpan(6,0,0));
+        _logger.LogDebug("From cache attempting to get cartitems for all users.");
         IEnumerable<CartItem>? cartItems = await _memoryCache.GetOrCreateAsync($"CartItemDetails", async entry =>
         {
             entry.AddExpirationToken(new CancellationChangeToken(Glob.detailsCancellationToken.Token));
             entry.RegisterPostEvictionCallback((key, value, reason, state) =>
             {
-                Console.Error.WriteLine($"Cache invalidation due to {reason} - {key} : {value}");
+                _logger.LogDebug("CartItems for All users has been invalidated due to {Reason} - {Key} : {Value}", reason, key, value);
             });
+            _logger.LogDebug("Cache miss, from database fetching cartitems for all users.");
             return await _cartItemRepository.GetAllAsync(q => q
                 .Include(c => c.Product)
                 .Include(c => c.User)
@@ -176,13 +199,18 @@ public class CartController
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Create(CartItemCreateDto cidto, string? returnPath)
     {
+        _logger.LogInformation("POST: Cart/Create.");
+
         CartItem cartItem = _mapper.Map<CartItemCreateDto, CartItem>(cidto);
         string? userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        _logger.LogDebug("Checking if user with \"{UserId}\" exists in database.", userId);
         bool userExists = await _userRepository.ExistsAsync(q => q.Where(u => u.Id == userId));
+        _logger.LogDebug("Checking if procduct with \"{ProductId}\" exists in database.", userId);
         bool productExists = await _productRepository.ExistsAsync(q => q.Where(p => p.Id == cartItem.ProductId));
         // Check that the keys are valid.
         if (string.IsNullOrWhiteSpace(userId) || !userExists || !productExists)
         {
+            _logger.LogError("The product id of \"{ProductId}\" and user Id of \"{UserId}\" do not exist in the database.", userId, cartItem.ProductId);
             return NotFound("Product and user specified does not exist..");
         }
 
@@ -190,12 +218,14 @@ public class CartController
 
         try
         {
+            _logger.LogDebug("Create CartItem in the database for user of id \"{UserId}\" and for product of id \"{ProductId}\".", cartItem.UserId, cartItem.ProductId);
             await _cartItemRepository.CreateAsync(cartItem);
         }
         catch (DbUpdateException)
         { }
 
         //Expire cache
+        _logger.LogDebug("Invalidate the cache for all cartItems and cartItems of user with id \"{UserId}\"", userId);
         if (Glob.userCancellationTokens.TryGetValue(userId, out var token))
         {
             await token.CancelAsync();
@@ -212,17 +242,22 @@ public class CartController
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Delete(CartItemDetailsDto cidto)
     {
+        _logger.LogInformation("POST: Cart/Delete.");
 
         if (cidto.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
         {
+            _logger.LogError("The user is not allowed to log from other user's cart.");
             return Unauthorized("You cannot only delete products from your own shopping cart");
         }
 
         var cartItem = _mapper.Map<CartItemDetailsDto, CartItem>(cidto);
+
+            _logger.LogDebug("Delete CartItem in the database for user of id \"{UserId}\" and for product of id \"{ProductId}\".", cartItem.UserId, cartItem.ProductId);
         await _cartItemRepository.DeleteCertainEntriesAsync(q => q
             .Where(c => c.UserId == cartItem.UserId && c.ProductId == cartItem.ProductId)
         );
 
+        _logger.LogDebug("Invalidate the cache for all cartItems and cartItems of user with id \"{UserId}\"", cartItem.UserId);
         if (Glob.userCancellationTokens.TryGetValue(cidto.UserId, out var token))
         {
             await token.CancelAsync();
