a288811c9039183d7fcda2b763c561c70dfc1e34
syildizz <saygin.efe@hotmail.com>
2024-10-11 14:28:55 +0300

    Added display names to CartItems.

--- SP_Shopping/Dtos/CartItemDetailsDto.cs
+++ SP_Shopping/Dtos/CartItemDetailsDto.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace SP_Shopping.Dtos;
@@ -7,7 +8,9 @@ public class CartItemDetailsDto
 {
     public string UserId { get; set; }
     public int ProductId { get; set; }
+    [DisplayName("Seller")]
     public string UserName { get; set; }
+    [DisplayName("Product Name")]
     public string ProductName { get; set; }
     [RegularExpression("([0-9]+)", ErrorMessage = "Please enter valid Number")]
     [Range(0, int.MaxValue)]
