69d93a7c3502fe03cd380721054a671b963dffe9
syildizz <saygin.efe@hotmail.com>
2024-10-21 14:02:20 +0300

    Get rid of unnecessary code from identity description razor page.

--- SP_Shopping/Areas/Identity/Pages/Account/Manage/Description.cshtml.cs
+++ SP_Shopping/Areas/Identity/Pages/Account/Manage/Description.cshtml.cs
@@ -2,112 +2,98 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
 using SP_Shopping.Models;
 
-namespace SP_Shopping.Areas.Identity.Pages.Account.Manage
+namespace SP_Shopping.Areas.Identity.Pages.Account.Manage;
+
+public class DescriptionModel : PageModel
 {
-    public class DescriptionModel : PageModel
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public DescriptionModel(UserManager<ApplicationUser> userManager)
     {
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly IEmailSender _emailSender;
+        _userManager = userManager;
+    }
 
-        public DescriptionModel(
-            UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager,
-            IEmailSender emailSender)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _emailSender = emailSender;
-        }
+    [TempData]
+    public string StatusMessage { get; set; }
 
-        [TempData]
-        public string StatusMessage { get; set; }
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    [BindProperty]
+    public InputModel Input { get; set; }
 
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public class InputModel
+    {
         /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
+        ///     My code that I added to ASP.NET Core Identity Scaffold.
         /// </summary>
-        [BindProperty]
-        public InputModel Input { get; set; }
+        [StringLength(1000, ErrorMessage = "The description can be at maximum 1000 characters long")]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+    }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public class InputModel
+    private void Load(ApplicationUser user)
+    {
+
+        Input = new InputModel
         {
-            /// <summary>
-            ///     My code that I added to ASP.NET Core Identity Scaffold.
-            /// </summary>
-            [StringLength(1000, ErrorMessage = "The description can be at maximum 1000 characters long")]
-            [Display(Name = "Description")]
-            public string Description { get; set; }
+            Description = user.Description
+        };
+
+    }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
         }
 
-        private async Task LoadAsync(ApplicationUser user)
+        Load(user);
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
         {
-
-            Input = new InputModel
-            {
-                Description = user.Description
-            };
-
+            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
         }
 
-        public async Task<IActionResult> OnGetAsync()
+        if (!ModelState.IsValid)
         {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            await LoadAsync(user);
+            Load(user);
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        var description = user.Description;
+        if (Input.Description != description)
         {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
+            user.Description = Input.Description;
+            var setDescriptionResult = await _userManager.UpdateAsync(user);
+
+            if (!setDescriptionResult.Succeeded)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                StatusMessage = "Unexpected error when trying to set account description.";
+                return RedirectToPage();
             }
-
-            if (!ModelState.IsValid)
-            {
-                await LoadAsync(user);
-                return Page();
-            }
-
-            var description = user.Description;
-            if (Input.Description != description)
-            {
-                user.Description = Input.Description;
-                var setDescriptionResult = await _userManager.UpdateAsync(user);
-
-                if (!setDescriptionResult.Succeeded)
-                {
-                    StatusMessage = "Unexpected error when trying to set account description.";
-                    return RedirectToPage();
-                }
-            }
-
-            StatusMessage = "Your account description has been updated.";
-            return RedirectToPage();
         }
 
+        StatusMessage = "Your account description has been updated.";
+        return RedirectToPage();
     }
+
 }
