936a37f039fae97f98ba53873740064ab0ba5cf0
syildizz <saygin.efe@hotmail.com>
2024-10-24 14:43:38 +0300

    Add key order to CartItem model.

---
+++ SP_Shopping/Data/Migrations/20241024113722_SpecifyOrderForPrimaryKeysForCartItemTable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SP_Shopping.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SpecifyOrderForPrimaryKeysForCartItemTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "CartItems",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)")
+                .Annotation("Relational:ColumnOrder", 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "CartItems",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)")
+                .OldAnnotation("Relational:ColumnOrder", 1);
+        }
+    }
+}
--- SP_Shopping/Models/CartItem.cs
+++ SP_Shopping/Models/CartItem.cs
@@ -8,13 +8,15 @@ namespace SP_Shopping.Models;
 public class CartItem
 {
     [Key]
-    [ForeignKey(nameof(Product))]
-    public int ProductId { get; set; }
-    public Product Product { get; set; }
-    [Key]
+    [Column(Order = 1)]
     [ForeignKey(nameof(User))]
     public string UserId { get; set; }
     public ApplicationUser User { get; set; }
+    [Key]
+    [Column(Order = 2)]
+    [ForeignKey(nameof(Product))]
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
     [RegularExpression("([0-9]+)", ErrorMessage = "Please enter valid Number")]
     [Range(0, int.MaxValue)]
     public int Count { get; set; }
