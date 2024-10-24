de2c4ef6dd883116c36c52cc3f7a4612030c32c9
syildizz <saygin.efe@hotmail.com>
2024-10-24 09:04:28 +0300

    Add generic ImageHandlerDefaulting class.

---
+++ SP_Shopping/Utilities/ImageHandler/ImageHandlerDefaulting.cs
@@ -0,0 +1,12 @@
+﻿namespace SP_Shopping.Utilities.ImageHandler;
+
+public class ImageHandlerDefaulting<TKey>(string folderPath, string defaultProp, string keyName, Func<TKey, string> identifier, string imgExtension = "png") : ImageHandlerDefaultingBase<TKey>(folderPath)
+{
+    protected override string DefaultProp => defaultProp;
+
+    protected override string KeyName => keyName;
+
+    protected override string ImgExtension => imgExtension;
+
+    protected override string Identifier(TKey key) => identifier(key);
+}
