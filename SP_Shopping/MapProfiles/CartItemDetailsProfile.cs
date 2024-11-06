f652077c4e0615ced1cb4359cbe521607f39caa1
syildizz <saygin.efe@hotmail.com>
2024-11-06 08:57:24 +0300

    Get rid of null warning.

--- SP_Shopping/MapProfiles/CartItemDetailsProfile.cs
+++ SP_Shopping/MapProfiles/CartItemDetailsProfile.cs
@@ -10,7 +10,7 @@ public class CartItemDetailsProfile : Profile
     {
         CreateMap<CartItem, CartItemDetailsDto>()
             .ForMember(c => c.UserName, opt => opt.MapFrom(c => c.User.UserName))
-            .ForMember(c => c.SubmitterName, opt => opt.MapFrom(c => c.Product.Submitter.UserName))
+            .ForMember(c => c.SubmitterName, opt => opt.MapFrom(c => (c.Product.Submitter == null) ? null : c.Product.Submitter.UserName))
             .ReverseMap();
     }
 
